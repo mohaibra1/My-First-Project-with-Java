e86168da59db6654e729d12b1fb1c0d8a6829705
Moha <mimohadinho@gmail.com>
2025-05-10 11:34:08 +0200

    Stage 1/3: Print the prices

---
+++ My First Project with Java/task/src/calculator/Main.java
@@ -0,0 +1,19 @@
+package calculator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        menu();
+    }
+
+    public static void menu(){
+        System.out.println("""
+                Prices:
+                Bubblegum: $2
+                Toffee: $0.2
+                Ice cream: $5
+                Milk chocolate: $4
+                Doughnut: $2.5
+                Pancake: $3.2""");
+    }
+}
